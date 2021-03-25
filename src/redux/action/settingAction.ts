98950fb972fc51fa497f9bc610426eac10c41a3f
Mikhail Hursky <kakai747o@gmail.com>
2021-03-25 22:07:49 +0300

    feat: add eslint

--- src/redux/action/settingAction.ts
+++ src/redux/action/settingAction.ts
@@ -1,4 +1,23 @@
-import { SETTING_MINUS, SETTING_PLUS, WORD_PLUS, WORD_MINUS, SOUND_CHANGE, CARD_PLUS, CARD_MINUS, btnAnswer,btnDelete,btnEasy,btnExample,btnGood,btnHard,btnMeaning,btnPicture,btnRepeate,btnTranscription,btnTranslate } from "../actionTypes";
+import {
+  SETTING_MINUS,
+  SETTING_PLUS,
+  WORD_PLUS,
+  WORD_MINUS,
+  SOUND_CHANGE,
+  CARD_PLUS,
+  CARD_MINUS,
+  btnAnswer,
+  btnDelete,
+  btnEasy,
+  btnExample,
+  btnGood,
+  btnHard,
+  btnMeaning,
+  btnPicture,
+  btnRepeate,
+  btnTranscription,
+  btnTranslate,
+} from '../actionTypes';
 
 export const counterPlus = () => ({
   type: SETTING_PLUS,
--- src/redux/reducer/rootReducer.ts
+++ src/redux/reducer/rootReducer.ts
@@ -1,10 +1,10 @@
-import { combineReducers } from "redux";
-import { settingReducer, settingState } from "./settingReducer";
+import { combineReducers } from 'redux';
+import { settingReducer, settingState } from './settingReducer';
 
 export interface State {
   setting: settingState;
 }
 
 export const rootReducer = combineReducers({
-  setting: settingReducer
+  setting: settingReducer,
 });
