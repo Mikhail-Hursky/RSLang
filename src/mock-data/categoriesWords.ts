930b95977840be3d0481a5f48c7e75dfdfa031b5
Mikhail Hursky <kakai747o@gmail.com>
2021-03-25 22:31:56 +0300

    Revert "feat: add eslint"
    This reverts commit 98950fb972fc51fa497f9bc610426eac10c41a3f.

--- src/mock-data/categoriesWords.ts
+++ src/mock-data/categoriesWords.ts
@@ -1,40 +1,32 @@
-interface Word {
-  id: number;
-  title: string;
-  color: string;
-}
-
-const CATEGORIES_WORDS: Word[] = [
+export const CATEGORIES_WORDS = [
   {
     id: 0,
     title: 'категория 1',
-    color: '#002140',
+    color: '#002140'
   },
   {
     id: 1,
     title: 'категория 2',
-    color: '#336da4',
+    color: '#336da4'
   },
   {
     id: 2,
     title: 'категория 3',
-    color: '#3c5062',
+    color: '#3c5062'
   },
   {
     id: 3,
     title: 'категория 4',
-    color: '#355572',
+    color: '#355572'
   },
   {
     id: 4,
     title: 'категория 5',
-    color: '#472961',
+    color: '#472961'
   },
   {
     id: 5,
     title: 'категория 6',
-    color: '#31233d',
+    color: '#31233d'
   },
 ];
-
-export default { CATEGORIES_WORDS };
