b2b5956c002fac5f754fc9a88d614ba077f5678e
Maryna Migacheva <m.migacheva@gmail.com>
2021-03-28 11:23:26 +0300

    feat: add data in page and pagination

--- src/components/category/Category.tsx
+++ src/components/category/Category.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState }  from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Route, Switch } from "react-router-dom";
 import { State } from "../../redux/reducer/rootReducer";
+import axios from "axios";
 
 import { Link } from "react-router-dom";
 import { Card  } from 'antd';
@@ -12,31 +13,68 @@ import "./Category.scss";
 export default function Category(props: any) {
 
   const group = useSelector((state: State) => state.setting.group);
-  const [response, setResponse] = useState<any | null>(null);
+  let [response, setResponse] = useState<any | null>(null);
+
+  const baseUrl = 'https://afternoon-falls-25894.herokuapp.com/words';
+  let pageToken = '0';
+  const groupToken = group;
+  let url = `${baseUrl}?page=${pageToken}&group=${groupToken}`;
 
   useEffect(() => {
-    setResponse(getData());
-  }, [setResponse]);
+      axios.get(url).then(function (response) {
+        console.log(response.data);
+        setResponse(response.data);
+      });
+    }, [setResponse]);
 
-    async function getData() {
-      const baseUrl = 'https://afternoon-falls-25894.herokuapp.com/words';
-      const pageToken = '0';
-      const groupToken = group;
-      const wordList = `${baseUrl}?page=${pageToken}&group=${groupToken}`;
-      const res = await fetch(wordList);
-      const data = await res.json();
-      console.log(data);
-      return data;
-    }
 
-    function onChange() {
+  if(response) {
+    response = response.map((item: any, i: any) => {
+        return (
+          <Card
+            key={i.toString()}
+            className={`item-${i} item`}
+          >
+            <p>{item.word}</p>
+            <p>{item.wordTranslate}</p>
+            <p>{item.transcription}</p>
+            <div className="button">
+              <button>прослушать</button>
+            </div>
+            <p>{item.textExample}</p>
+            <p>{item.textExampleTranslate}</p>
+          </Card>
+        );
+      });
+  }
 
+
+    function onChange(e: any) {
+      window.scrollTo(0, 0);
+      console.log(e);
+      pageToken = e;
+      url = `${baseUrl}?page=${pageToken}&group=${groupToken}`;
+      axios.get(url).then(function (response) {
+        console.log(response.data);
+        setResponse(response.data);
+      });
+      return e;
     }
 
     return(
       <div className="list-word">
-        <h2>categories {group + 1}</h2>
-        <Pagination onChange={onChange} total={600} />
+        <h2>category {group + 1}</h2>
+        <div className="wrap-list-word">
+          {response}
+        </div>
+        <Pagination
+          onChange={onChange}
+          total={600}
+          defaultPageSize={20}
+          defaultCurrent={1}
+          showSizeChanger={false}
+          showQuickJumper
+        />
       </div>
     )
 }
