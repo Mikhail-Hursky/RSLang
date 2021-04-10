ad11e309652ffaa57fd67ee72a448c618124b8d0
fedorovichpavel <fedorovich.pavel@mail.ru>
2021-04-10 22:24:22 +0300

    fix:exit fullscreen on firefox browser

--- src/components/statistic/Statistic.tsx
+++ src/components/statistic/Statistic.tsx
@@ -1,131 +1,143 @@
-import { Collapse, Divider, List, Typography } from 'antd';
-import { Content } from 'antd/lib/layout/layout';
-import React, { useEffect, useState } from 'react';
-import { useSelector } from 'react-redux';
-import { Api } from '../../api/Api';
-import { State } from '../../redux/reducer/rootReducer';
+import { Collapse, Divider, List, Typography } from "antd";
+import { Content } from "antd/lib/layout/layout";
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { Api } from "../../api/Api";
+import { State } from "../../redux/reducer/rootReducer";
 import "./Statistic.scss";
 
-function Spoiler({games, data, percent, streak}: any) {
- 
- const { Panel } = Collapse;
+function Spoiler({ games, data, percent, streak }: any) {
+  const { Panel } = Collapse;
 
+  const text = [
+    "Изучено слов - ",
+    "Процент правильных ответов - ",
+    "Серия правильных ответов - ",
+  ];
 
-
-
- const text = ['Изучено слов - ',
- 'Процент правильных ответов - ',
- 'Серия правильных ответов - '];
-
- return (
-  <Collapse accordion>
-    {
-    games.map((game:string, index:number) => (
-      <Panel header={`${game} game`} key={game}>
-      <p><List.Item>
-       {text[0] + data[index][game]}
-     </List.Item></p>
-     <p><List.Item>
-       {text[1] + percent[game]["percent"] + '%'}
-     </List.Item></p>
-     <p><List.Item>
-       {text[2] + streak[game]}
-     </List.Item></p>
-    </Panel>
-    ))}
-  </Collapse>
- )
+  return (
+    <Collapse accordion>
+      {games.map((game: string, index: number) => (
+        <Panel header={`${game} game`} key={game}>
+          <p>
+            <List.Item>{text[0] + data[index][game]}</List.Item>
+          </p>
+          <p>
+            <List.Item>{text[1] + percent[game]["percent"] + "%"}</List.Item>
+          </p>
+          <p>
+            <List.Item>{text[2] + streak[game]}</List.Item>
+          </p>
+        </Panel>
+      ))}
+    </Collapse>
+  );
 }
 
 export default function Statistic() {
   const { message, token, userId } = useSelector((state: State) => state.user);
   const games = ["Audiocall", "Savannah", "Sprint", "Our"];
-  const [data, setData] = useState<any>({0: {"Savannah": 0} ,1:{"Audiocall": 0},2:{"Sprint": 0},3:{"Our": 0}}); 
+  const [data, setData] = useState<any>({
+    0: { Savannah: 0 },
+    1: { Audiocall: 0 },
+    2: { Sprint: 0 },
+    3: { Our: 0 },
+  });
   const [words, setWords] = useState<any>({
-    "Sprint": 0,
-    "Savannah": 0,
-    "Our": 0,
-    "Audiocall": 0
-})
- const [percent, setPercent] = useState<any>({"Savannah": {"percent": 0},"Audiocall": {"percent": 0},"Sprint": {"percent": 0},"Our": {"percent": 0}}); 
- const [streak, setStreak] = useState<any>({
-  Sprint: 0,
-  Savannah: 0,
-  Our: 0,
-  Audiocall: 0,
-});
+    Sprint: 0,
+    Savannah: 0,
+    Our: 0,
+    Audiocall: 0,
+  });
+  const [percent, setPercent] = useState<any>({
+    Savannah: { percent: 0 },
+    Audiocall: { percent: 0 },
+    Sprint: { percent: 0 },
+    Our: { percent: 0 },
+  });
+  const [streak, setStreak] = useState<any>({
+    Sprint: 0,
+    Savannah: 0,
+    Our: 0,
+    Audiocall: 0,
+  });
 
-
-  useEffect(()=> {
-    Api.getUserStat(userId, token).then((response)=> {
+  useEffect(() => {
+    Api.getUserStat(userId, token).then((response) => {
       console.log(response);
       if (response.status === 200) {
         console.log(response);
         const obj = {};
-        const objGames = games.map((e:any) => {
+        const objGames = games.map((e: any) => {
           let game = response.data.optional.words[e];
           let count = 0;
           for (let key in game) {
-             count++;
+            count++;
           }
-          return {[e]: count};
-        })
+          return { [e]: count };
+        });
         setWords(response.data.optional.words);
         setStreak(response.data.optional.streak);
-        setData({...objGames});
+        setData({ ...objGames });
         setPercent(response.data.optional.percent);
       }
     });
-   }, []);
+  }, []);
 
- let sumPercent = 0;
- let count = 0;
- for (let key in percent) { 
-   if (percent[key].percent === 0) continue
-   count++;
-  sumPercent += +percent[key].percent;
-  sumPercent = sumPercent/count;
- }
+  let sumPercent = 0;
+  let count = 0;
+  for (let key in percent) {
+    if (percent[key].percent === 0) continue;
+    count++;
+    sumPercent += +percent[key].percent;
+    sumPercent = sumPercent / count;
+  }
 
- let newWords:any = [];
- console.log('words ', words)
- for (let key in words) { 
-  if (words[key] === 0) continue
+  let newWords: any = [];
+  console.log("words ", words);
+  for (let key in words) {
+    if (words[key] === 0) continue;
     const wordGame = [];
-    for (let i in words[key]) {wordGame.push(words[key][i]);}
-    console.log(wordGame)
-  const idNewWords = newWords ? newWords.map((e:any) => e["id"]) : [];
-  const newWordsFiltred = wordGame.filter((e:any) => !idNewWords.includes(e["id"]));
-  newWords = [...newWords, ...newWordsFiltred];
- }
+    for (let i in words[key]) {
+      wordGame.push(words[key][i]);
+    }
+    console.log(wordGame);
+    const idNewWords = newWords ? newWords.map((e: any) => e["id"]) : [];
+    const newWordsFiltred = wordGame.filter(
+      (e: any) => !idNewWords.includes(e["id"])
+    );
+    newWords = [...newWords, ...newWordsFiltred];
+  }
 
- console.log(newWords);
+  console.log(newWords);
 
- const dataText = [
-  `Слов изучено сегодня - ${newWords.length}`,
-  `Процент правильных ответов сегодня - ${sumPercent.toFixed(2)}%`,
-];
+  const dataText = [
+    `Слов изучено сегодня - ${newWords.length}`,
+    `Процент правильных ответов сегодня - ${sumPercent.toFixed(2)}%`,
+  ];
 
-console.log()
+  console.log();
 
- return (
-  <Content className="site-layout" style={{ padding: '0 50px', marginTop: 48 }}>
-  <div className="site-layout-background" style={{ padding: 24, minHeight: 550 }}>
-  <Divider orientation="left">Статистика</Divider>
-    <Spoiler games={games} data={data} percent={percent} streak={streak}/>
-    <List
-      // header={<div>Сегодня</div>}
-      // footer={<div>Footer</div>}
-      bordered
-      dataSource={dataText}
-      renderItem={item => (
-        <List.Item>
-          {item}
-        </List.Item>
-      )}
-    />
-  </div>
-</Content>
- )
+  return (
+    <Content
+      className="site-layout"
+      style={{ padding: "0 50px", marginTop: 48 }}
+    >
+      <div
+        className="site-layout-background"
+        style={{ padding: 24, minHeight: 550 }}
+      >
+        <Divider orientation="left">Статистика</Divider>
+        <Spoiler games={games} data={data} percent={percent} streak={streak} />
+        <List
+          // header={<div>Сегодня</div>}
+          // footer={<div>Footer</div>}
+          className="stat__list"
+          bordered
+          dataSource={dataText}
+          renderItem={(item) => <List.Item>{item}</List.Item>}
+        />
+      </div>
+    </Content>
+  );
 }
-
--- src/components/statisticModal/StatisticModal.tsx
+++ src/components/statisticModal/StatisticModal.tsx
@@ -1,107 +1,173 @@
 import React, { useEffect } from "react";
-import { Modal, Progress, message, Button, Space } from 'antd';
+import { Modal, Progress, message, Button, Space } from "antd";
 import { soundWord } from "../../sound/sound";
-import './StatisticModal.scss';
+import "./StatisticModal.scss";
 import { exitFullscreen } from "../../fullscreen";
 import { DeleteOutlined } from "@ant-design/icons";
 import { useSelector } from "react-redux";
 import { State } from "../../redux/reducer/rootReducer";
 import { Api } from "../../api/Api";
 
-function StatisticModal({words, setStart, game, streak}:any) {
-const { token, userId } = useSelector((state: State) => state.user);
-const userMessage = useSelector((state: State) => state.user.message);
+function StatisticModal({ words, setStart, game, streak }: any) {
+  const { token, userId } = useSelector((state: State) => state.user);
+  const userMessage = useSelector((state: State) => state.user.message);
 
- const handleOk = (e:any) => {
-  setStart(false);
-};
+  const handleOk = (e: any) => {
+    setStart(false);
+  };
 
-useEffect(()=> {
-  let percent = +(words[0].length/words[2] * 100).toFixed(2);
-  exitFullscreen('game_fullscreen');
-  Api.getUserStat(userId, token).then((response)=> {
-    const newWords:any = [];
-    let newWordObj;
-      const allWords = response.data.optional.words;
-        const wordsArray = response.data.optional.words[game] === 0 ? {} : response.data.optional.words[game];
+  useEffect(() => {
+    let percent = +((words[0].length / words[2]) * 100).toFixed(2);
+    exitFullscreen("game_fullscreen");
+    Api.getUserStat(userId, token).then((response) => {
+      if (response.status === 200) {
+        const newWords: any = [];
+        let newWordObj;
+        const allWords = response.data.optional.words;
+        const wordsArray =
+          response.data.optional.words[game] === 0
+            ? {}
+            : response.data.optional.words[game];
         for (let key in wordsArray) {
           newWords.push(wordsArray[key]);
         }
-    words[0].forEach((e:any) => {
-      if (!newWords.includes(e["id"])) { newWords.push(e["id"]); }
+        words[0].forEach((e: any) => {
+          if (!newWords.includes(e["id"])) {
+            newWords.push(e["id"]);
+          }
+        });
+        allWords[game] = { ...newWords };
+        newWordObj = { ...allWords };
+        console.log(newWordObj);
+        console.log(words[0]);
+        if (response.data.optional.percent[game].percent !== 0) {
+          percent = +(
+            (response.data.optional.percent[game].percent + percent) /
+            2
+          ).toFixed(2);
+        }
+        let streakStat = streak[0] > streak[1] ? streak[0] : streak[1];
+        const allStreak = response.data.optional.streak;
+        let newStreakObj = allStreak;
+        console.log(response.data.optional.streak);
+        if (response.data.optional.streak[game] < streakStat) {
+          newStreakObj = { ...allStreak, [game]: streakStat };
+        }
+        console.log(streakStat);
+        const allPercent = response.data.optional.percent;
+        const newPercentObj = { ...allPercent, [game]: { percent } };
+        console.log(
+          token,
+          userId,
+          words[0].length,
+          newWordObj,
+          newPercentObj,
+          newStreakObj
+        );
+        Api.setUserStat(
+          token,
+          userId,
+          words[0].length,
+          newWordObj,
+          newPercentObj,
+          newStreakObj
+        );
+      }
     });
-    allWords[game] = {...newWords};
-    newWordObj = {...allWords};
-    console.log(newWordObj);
-    console.log(words[0]);
-    if (response.data.optional.percent[game].percent !== 0) {
-      percent = +((response.data.optional.percent[game].percent + percent) / 2).toFixed(2);
-    }
-    let streakStat = streak[0] > streak[1] ? streak[0] : streak[1];
-    const allStreak = response.data.optional.streak;
-    let newStreakObj = allStreak;
-    console.log(response.data.optional.streak);
-    if (response.data.optional.streak[game] < streakStat) {
-      newStreakObj = {...allStreak, [game]:streakStat};
-    }
-    console.log(streakStat);
-    const allPercent = response.data.optional.percent;
-    const newPercentObj = {...allPercent, [game]:{percent}};
-    console.log(token, userId, words[0].length, newWordObj, newPercentObj, newStreakObj);
-    Api.setUserStat(token, userId, words[0].length, newWordObj, newPercentObj, newStreakObj);
-   });
+  }, []);
 
-}, []);
+  const successMessage = () => {
+    message.success("Слово успешно удалено!");
+  };
 
-const successMessage = () => {
-  message.success('Слово успешно удалено!');
-};
+  function onClick(event: any, i: number, words: any) {
+    Api.setUserWord(token, userId, words[0]["id"], "DELETED");
+    event.target.remove();
+    successMessage();
+  }
 
-function onClick(event:any, i:number, words:any) {
-  Api.setUserWord(token, userId, words[0]["id"], "DELETED");
-  event.target.remove();
-  successMessage();
-}
- 
-return (
-  <> 
-    <Modal
-     className="statistic_modal"
-      title="Статистика игры"
-      visible={true}
-      onOk={handleOk}
-      okButtonProps={{ disabled: false }}
-      cancelButtonProps={{ disabled: true, style:{display: 'none'} }}
-    >
-      <Progress
-      type="circle"
-      strokeColor={'red'}
-      percent={+((words[0].length+words[1].length)/words[2] * 100).toFixed(2)}
-      success={{ percent: +(words[0].length/words[2] * 100).toFixed(2) }}
-      style={{display:'flex', justifyContent: 'center', marginBottom: '15px'}}
-      format={(percent, successPercent) => `${successPercent}%`}
-    />
-      <p className="statisticModal_words_title" style={{color:'#ff0707'}}>Я не знаю {words[1].length}</p>
-     {words[1].map((e:any, i:any)=> {
-      return (
-      <div className="statistic_modal_word" key={i} >
-       <img onClick={() => soundWord(`https://lang-en.herokuapp.com/${e['audio']}`)} src="../../img/sound.svg" alt="Озвучить"/>
-        <p className="statisticModal_word_text">{`${e['word']} - ${e['wordTranslate']}`}</p>
-        {userMessage === "Authenticated" ? <DeleteOutlined className="delete_word" onClick={(event:any) => onClick(event, i, words[1])} /> : <></>}
-      </div> )
-     }) }
-      <p className="statisticModal_words_title" style={{color:'#02da3a'}}>Я знаю {words[0].length}</p>
-      {words[0].map((e:any, i:any)=> {
-       return (
-      <div className="statistic_modal_word" key={i}>
-       <img onClick={() => soundWord(`https://lang-en.herokuapp.com/${e['audio']}`)} src="../../img/sound.svg" alt="Озвучить" />
-      <p className="statisticModal_word_text"> {`${e['word']} - ${e['wordTranslate']}`} </p>
-      {userMessage === "Authenticated" ? <DeleteOutlined className="delete_word" onClick={(event:any) => onClick(event, i, words[0])} /> : <></>}
-      </div> )
-     }) }
-    </Modal>
-  </>
-);
+  return (
+    <>
+      <Modal
+        className="statistic_modal"
+        title="Статистика игры"
+        visible={true}
+        onOk={handleOk}
+        okButtonProps={{ disabled: false }}
+        cancelButtonProps={{ disabled: true, style: { display: "none" } }}
+      >
+        <Progress
+          type="circle"
+          strokeColor={"red"}
+          percent={
+            +(((words[0].length + words[1].length) / words[2]) * 100).toFixed(2)
+          }
+          success={{
+            percent: +((words[0].length / words[2]) * 100).toFixed(2),
+          }}
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            marginBottom: "15px",
+          }}
+          format={(percent, successPercent) => `${successPercent}%`}
+        />
+        <p className="statisticModal_words_title" style={{ color: "#ff0707" }}>
+          Я не знаю {words[1].length}
+        </p>
+        {words[1].map((e: any, i: any) => {
+          return (
+            <div className="statistic_modal_word" key={i}>
+              <img
+                onClick={() =>
+                  soundWord(`https://lang-en.herokuapp.com/${e["audio"]}`)
+                }
+                src="../../img/sound.svg"
+                alt="Озвучить"
+              />
+              <p className="statisticModal_word_text">{`${e["word"]} - ${e["wordTranslate"]}`}</p>
+              {userMessage === "Authenticated" ? (
+                <DeleteOutlined
+                  className="delete_word"
+                  onClick={(event: any) => onClick(event, i, words[1])}
+                />
+              ) : (
+                <></>
+              )}
+            </div>
+          );
+        })}
+        <p className="statisticModal_words_title" style={{ color: "#02da3a" }}>
+          Я знаю {words[0].length}
+        </p>
+        {words[0].map((e: any, i: any) => {
+          return (
+            <div className="statistic_modal_word" key={i}>
+              <img
+                onClick={() =>
+                  soundWord(`https://lang-en.herokuapp.com/${e["audio"]}`)
+                }
+                src="../../img/sound.svg"
+                alt="Озвучить"
+              />
+              <p className="statisticModal_word_text">
+                {" "}
+                {`${e["word"]} - ${e["wordTranslate"]}`}{" "}
+              </p>
+              {userMessage === "Authenticated" ? (
+                <DeleteOutlined
+                  className="delete_word"
+                  onClick={(event: any) => onClick(event, i, words[0])}
+                />
+              ) : (
+                <></>
+              )}
+            </div>
+          );
+        })}
+      </Modal>
+    </>
+  );
 }
 
 export default StatisticModal;
